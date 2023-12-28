6d6e5f421489064960293c44ac0cd6fb639ff644
PedroAugusto2305 <p.augusto_23@outlook.com>
2023-12-28 13:01:30 -0300

    Api consuming

--- src/js/script.js
+++ src/js/script.js
@@ -35,3 +35,46 @@ function removeActiveLink() {
       .attachEvent("onmousewheel", scrollHorizontally);
   }
 })();
+
+// API consuming
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const gridContainer = document.querySelector(".characters-page__grid");
+
+  const fetchCharacters = async (start, end) => {
+    const characters = [];
+    for (let i = start; i <= end; i++) {
+      try {
+        const response = await fetch(
+          `https://rickandmortyapi.com/api/character/${i}`
+        );
+        const characterData = await response.json();
+        characters.push(characterData);
+      } catch (error) {
+        console.error("Erro ao carregar personagem:", error);
+      }
+    }
+    return characters;
+  };
+
+  const createCharacterCard = (character) => {
+    const card = document.createElement("div");
+    card.className = "characters-page__card__container";
+
+    card.innerHTML = `
+          <article class="characters-page__card" style="background-image: linear-gradient(180deg, #06090f00 0%, #06090f 80.79%), url('${character.image}');">
+            <h3 class="characters-page__card__subtitle">${character.species}</h3>
+          </article>
+          <h2 class="characters-page__card__title" style="text-align: center;">${character.name}</h2>
+    `;
+
+    return card;
+  };
+
+  const characters = await fetchCharacters(1, 12);
+  characters.forEach((character) => {
+
+    const characterCard = createCharacterCard(character);
+    gridContainer.appendChild(characterCard);
+  });
+});
